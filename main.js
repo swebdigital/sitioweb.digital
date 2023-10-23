2efd22fb8b9f35288c41bf676e915eb9517e39a8
swebdigital <148819633+swebdigital@users.noreply.github.com>
2023-10-23 18:33:10 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+document.getElementById("fecha").addEventListener("click", function () {
+    const today = new Date();
+    const year = today.getFullYear();
+    const month = today.getMonth() + 1;
+    const day = today.getDate();
+    const formattedDate = `${year}-${month.toString().padStart(2, '0')}-${day.toString().padStart(2, '0')}`;
+    document.getElementById("fecha").setAttribute("min", formattedDate);
+});
